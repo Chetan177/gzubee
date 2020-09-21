e9d740029a525d125a72604a8d6ca1cca36ce17c
chetan177 <chetanpandey177@gmail.com>
2020-09-21 21:25:07 +0530

    logger added in token.js

--- restService/routes/token.js
+++ restService/routes/token.js
@@ -1,6 +1,8 @@
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
+const { loggers } = require('winston')
 
+const logger = loggers.get('guzbeeLogger')
 
 dotenv.config();
 
@@ -10,29 +12,30 @@ function generateAccessToken(username) {
   var signOptions = {
     expiresIn:  "30m"
    };
-
+  
   newToken = jwt.sign(username, secret, signOptions)
+  logger.info(`New token generated for user: ${username.username} token: ${newToken}`)
   return newToken;
 }
 
 authenticateToken = function (req, res, next) {
-  // Gather the jwt access token from the request header
+  
   const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
   if (token == null) return res.sendStatus(401) 
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    console.log(err)
+    logger.error(`Token verification error: ${err}`)
     if (err) return res.sendStatus(403)
     req.user = user
-    next() // pass the execution off to whatever request the client intended
+    next() 
   })
 }
 
 getToken = function (req, res) {
   res.status(200);
   user = {username : req.body.username}
-  const token = generateAccessToken(user);
+  token = generateAccessToken(user);
   res.json({
     msg: "Access Token Generated",
     token: token,
