45fcdb59de0c7993aaedb86ff147f2d366db714e
chetan177 <chetanpandey177@gmail.com>
2020-09-21 20:15:38 +0530

    lint fix

--- restService/index.js
+++ restService/index.js
@@ -1,40 +1,36 @@
-const winston = require('winston');
+const winston = require("winston");
 
 // Modules
 const express = require("express");
 const bodyParser = require("body-parser");
-const customMiddleware = require("./middleware/errorHandler")
+const customMiddleware = require("./middleware/errorHandler");
 // const logger = require("./middleware/log")
 
 // Config Constants
 const defualtRoute = "/v01/";
-const logFile = "../log/guzbee.log"
+const logFile = "../log/guzbee.log";
 
 // TODO Unified
 var logger = winston.createLogger({
-    format: winston.format.json(),
-    defaultMeta: { service: 'rest-service' },
-    transports: [
-      new winston.transports.File({ filename: logFile }),
-    ],
-  });
-  
+  format: winston.format.json(),
+  defaultMeta: { service: "rest-service" },
+  transports: [new winston.transports.File({ filename: logFile })],
+});
+
 // Variables
 var app = express();
 
 // Routes paths
 const token = require("./routes/token");
-const health = require("./routes/health")
-
+const health = require("./routes/health");
 
 // Middlewares
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
 
 // * Routes * //
-app.get(defualtRoute+"health",health)
-app.post("/getAuthToken/", token.getToken);
-
+app.get(defualtRoute + "health", health);
+app.post(defualtRoute + "getAuthToken/", token.getToken);
 
 // Error handlers
 app.use(customMiddleware.err404);
