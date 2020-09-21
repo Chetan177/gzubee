f0b723379bffd52bf5e0d63e5bcffa9239dd7002
chetan177 <chetanpandey177@gmail.com>
2020-09-21 21:24:31 +0530

    log middleware created

--- restService/middleware/errorHandler.js
+++ restService/middleware/errorHandler.js
@@ -1,18 +1,11 @@
 // Modules
-// TODO Unified
-var winston = require("winston");
-const logFile = "../../log/guzbee.log"
-var logger = winston.createLogger({
-    format: winston.format.json(),
-    defaultMeta: { service: 'rest-service' },
-    transports: [
-      new winston.transports.File({ filename: logFile }),
-    ],
-  });
+const { loggers } = require('winston')
+
+const logger = loggers.get('guzbeeLogger')
 
 error404 = function (req, res, next) {
   res.status(404);
-  logger.info("Error in request ");
+  logger.error(`Error in request method: ${req.method} path: ${req.originalUrl}`);
   res.json({
     error: "Not found",
   });
@@ -21,7 +14,7 @@ error404 = function (req, res, next) {
 
 error500 = function (err, req, res, next) {
   res.status(err.status || 500);
-  logger.error("%s %d %s", req.method, res.statusCode, err.message);
+  logger.error( `Error in request method: ${req.method} path: ${req.originalUrl} status: ${res.statusCode} error: ${err.message}`);
   res.json({
     error: err.message,
   });
